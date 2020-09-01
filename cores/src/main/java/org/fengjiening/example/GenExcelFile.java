9bcd5b1152832dfecb4cde4e0faff8f54d330d35
fengjiening <271760513@qq.com>
2020-09-01 18:17:14 +0800

    添加 自动生成Excle 文件，方便日常开发

---
+++ cores/src/main/java/org/fengjiening/example/GenExcelFile.java
@@ -0,0 +1,40 @@
+package org.fengjiening.example;
+
+import org.fengjiening.excel.model.ExcelEntityView;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class GenExcelFile
+{
+    public static List<User> list = new ArrayList<>();
+    static {
+        list.add(new User("上海", "小名", 17));
+        list.add(new User("北京", "小红", 18));
+        list.add(new User("深圳", "小蓝", 19));
+        list.add(new User("广州", "小灰", 20));
+        list.add(new User("杭州", "小黄", 21));
+        list.add(new User("贵阳", "小白", 22));
+    }
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testExe() throws Exception {
+        //模拟数据 fileName,Collection exportList ,Class entityClass,String path
+        new ExcelEntityView(list,User.class,"c://","文件","这是一个标题").toExcelFile();
+    }
+}
